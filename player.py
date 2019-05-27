17a4c6429fe18056faa415f80254d04f54a171da
Lizi Boros <boroslizi@gmail.com>
2019-05-27 11:33:41 +0200

    Betting logic test

--- player.py
+++ player.py
@@ -3,6 +3,15 @@ class Player:
     VERSION = "V 18"
 
     def betRequest(self, game_state):
+        players = game_state['players']
+
+        for player in players:
+            if player['name'] == "Successful cows":
+                our_cards = player['hole_cards']
+                for card in our_cards:
+                    if card['rank'] in "JQKA":
+                        return game_state['current_buy_in'] - player['bet'] + game_state['minimum_raise']
+
         return 30
 
     def showdown(self, game_state):
