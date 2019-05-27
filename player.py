b77bd6881283f27b2005240af0f88948a1004d18
Lizi Boros <boroslizi@gmail.com>
2019-05-27 11:35:55 +0200

    Betting logic by id

--- player.py
+++ player.py
@@ -4,15 +4,16 @@ class Player:
 
     def betRequest(self, game_state):
         players = game_state['players']
+        id = game_state['in_action']
 
         for player in players:
-            if player['name'] == "Successful cows":
+            if player['id'] == id:
                 our_cards = player['hole_cards']
                 for card in our_cards:
                     if card['rank'] in "JQKA":
                         return game_state['current_buy_in'] - player['bet'] + game_state['minimum_raise']
 
-        return 30
+        # return 30
 
     def showdown(self, game_state):
         pass
